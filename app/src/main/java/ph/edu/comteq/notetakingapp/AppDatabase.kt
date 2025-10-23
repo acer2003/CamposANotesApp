8cda067d5fabfbadbbba56411cc847f29af595a0
acer2003 <camposacer2@gmail.com>
2025-10-23 20:07:26 +0800

    Initial commit
    Created Note Entity, Dao, Database, viewmodel and Note list UI

---
+++ app/src/main/java/ph/edu/comteq/notetakingapp/AppDatabase.kt
@@ -0,0 +1,33 @@
+package ph.edu.comteq.notetakingapp
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Note::class],
+    version = 1,
+    exportSchema = true
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                val newInstance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "notes_database"
+                ).build()
+                instance = newInstance
+                newInstance
+            }
+        }
+    }
+}
